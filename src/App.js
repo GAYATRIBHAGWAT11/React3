f5bac8ea561663f9a102fa45e9d8d37dbac1c1f6
Admin <admin@Admins-MacBook-Air.local>
2022-09-20 15:03:54 +0530

    adding some new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Greeting } from './components/greeting';
+import Landing from './components/Landing';
+import { Names } from './components/Names';
+// import Maincontent from './components/MainContent/Maincontent';
+// import Person from './components/Person';
+// import Navbar from './components/Navbar';
+// import textForm from './components/textForm';
+// import {counter} from './components/counter';
+// import Profile from './components/Profile'
+import Navbar from './InstaClone/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// Toggle=> classname
+ function App(){
+   return(
+     <div>
+       {/* <Landing/> */}
+       {/* <Names/> */}
+       <Greeting/>
+     </div>
+   );
+ }
+ export default App;
