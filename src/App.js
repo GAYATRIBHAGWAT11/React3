a58978dc6ef3c551d5ecb5e921a57767d947df3b
Admin <admin@Admins-MacBook-Air.local>
2022-09-21 15:03:34 +0530

    use of useState(hooks)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Greeting } from './components/greeting';
-import Landing from './components/Landing';
-import { Names } from './components/Names';
+// import FavoriteColor from './components/Usestate';
+import Counter from './components/UsestateCounter';
+// import LoginUi from './components/LoginPage/Login';
+// import { Greeting } from './components/greeting';
+// import Landing from './components/Landing';
+// import Person from './components/Person';
+// import { Names } from './components/Names';
 // import Maincontent from './components/MainContent/Maincontent';
 // import Person from './components/Person';
 // import Navbar from './components/Navbar';
 // import textForm from './components/textForm';
 // import {counter} from './components/counter';
 // import Profile from './components/Profile'
-import Navbar from './InstaClone/Navbar';
+// import Navbar from './InstaClone/Navbar';
 
 // Toggle=> classname
  function App(){
@@ -17,7 +21,11 @@ import Navbar from './InstaClone/Navbar';
      <div>
        {/* <Landing/> */}
        {/* <Names/> */}
-       <Greeting/>
+       {/* <Greeting/> */}
+       {/* <Person/> */}
+       {/* <LoginUi/> */}
+       {/* <FavoriteColor/> */}
+      <Counter/>
      </div>
    );
  }
